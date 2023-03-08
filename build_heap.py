33e47a9d094563fc6af929f0444f0350a6f56bc0
JurisRoga <124889862+JurisRoga@users.noreply.github.com>
2023-03-08 19:00:30 +0000

    test4

--- build_heap.py
+++ build_heap.py
@@ -6,11 +6,11 @@ def build_heap(data):
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
     n = len(data)
-    for i in range(n//2-1, -1, -1):
+    for i in range(n // 2-1, -1, -1):
         j = 1
-        while 2*j+1<n:
+        while 2*j+1 < n:
             k = 2*j+1
-            if k+1 < n and data[k+1] < data[k]:
+            if k+1 < n and data[k] < data[k+1]:
                 k += 1
             if data[j] <= data[k]:
                 break
